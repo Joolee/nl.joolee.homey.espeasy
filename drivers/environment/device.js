9e7b425433138d19bd1ab2bf419ccd518a878059
Joolee <github@joolee.nl>
2020-06-10 13:10:50 +0200

    Fix class name of Environment sensors

--- drivers/environment/device.js
+++ drivers/environment/device.js
@@ -2,6 +2,6 @@
 
 const SensorDevice = require('/lib/SensorDevice.js');
 
-module.exports = class Sensor_Switch extends SensorDevice {
+module.exports = class Environment extends SensorDevice {
 
 }
