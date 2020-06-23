2541c0d7c759f31d6fb6d3affc7cb0251e4da99b
Joolee <github@joolee.nl>
2020-06-23 11:03:39 +0200

    Add support for simple dummy device

--- drivers/input_switch/driver.js
+++ drivers/input_switch/driver.js
@@ -7,16 +7,20 @@ module.exports = class Sensor_Switch extends SensorDriver {
 	onInit() {
 		this.taskTypes = [
 			{
-				"name": 'Switch input - Switch',
+				"name": "Switch input - Switch",
 				"plugin": 1
 			},
 			{
-				"name": 'Switch input - MCP23017',
+				"name": "Switch input - MCP23017",
 				"plugin": 9
 			},
 			{
-				"name": 'Switch input - PCF8574',
+				"name": "Switch input - PCF8574",
 				"plugin": 19
+			},
+			{
+				"name": "Generic - Dummy Device",
+				"plugin": 33
 			}
 		]
 
@@ -24,7 +28,7 @@ module.exports = class Sensor_Switch extends SensorDriver {
 			{
 				"name": "State",
 				"capability": cap =>
-					!cap.setable && cap.getable && cap.type == 'boolean',
+					!cap.setable && cap.getable && cap.type == "boolean",
 				"validate": (taskVal, value) => {
 					if (value == -1)
 						return Homey.__("sensor.io_board_offline")
