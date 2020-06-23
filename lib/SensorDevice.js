Joolee/nl.joolee.homey.espeasy

commit 7b2939ab2956a0c46a8b06498a7938fe366794c8

Fixed settings sensor values

lib/SensorDevice.js
@@ -92,14 +92,21 @@ module.exports = class SensorDevice extends GeneralDevice {
 			return false;
 		}
 
-		const settings = this.getDriver().values[taskValue.ValueNumber - 1];
-		if (!settings) {
+		const taskSettings = this.getDriver().taskTypes.find(t => t.name == this.task.Type);
+		if (!taskSettings) {
+			this.log('No settings found for this task', key);
+			return false;
+		}
+
+		const capabilityName = taskSettings.values[taskValue.ValueNumber - 1];
+		const capabilitySettings = this.getDriver().values.find(v => v.name == capabilityName);
+		if (!capabilitySettings) {
 			this.log('No capability configured for value', key);
 			return false;
 		}
 
-		if (typeof (settings.validate) == "function") {
-			const response = settings.validate(taskValue, value);
+		if (typeof (capabilitySettings.validate) == "function") {
+			const response = capabilitySettings.validate(taskValue, value);
 			if (typeof (response) == "string") {
 				this.log("Value did not validate:", value, response);
 				this.setUnavailable(response);
@@ -111,7 +118,7 @@ module.exports = class SensorDevice extends GeneralDevice {
 			}
 		}
 
-		let capability = settings.capability;
+		let capability = capabilitySettings.capability;
 		if (typeof (capability) == "function") {
 			capability = this.getSetting('capability-' + (taskValue.ValueNumber - 1));
 		}
@@ -124,7 +131,11 @@ module.exports = class SensorDevice extends GeneralDevice {
 		const oldValue = this.getCapabilityValue(capability);
 		if (oldValue != value) {
 			this.log(`Value '${key}' (${capability}) set from ${oldValue} to ${value}`)
-			this.setCapabilityValue(capability, value);
+			this.setCapabilityValue(capability, value)
+				.catch((...args) => {
+					args.push(capability, value);
+					this.log.apply(args)
+				});
 		}
 	}
 

commit 10d3381ac696747ca843c0c64565eec94d453d04

Fix bug for input devices introduced in commit 7b2939a

lib/SensorDevice.js
@@ -98,7 +98,13 @@ module.exports = class SensorDevice extends GeneralDevice {
 			return false;
 		}
 
-		const capabilityName = taskSettings.values[taskValue.ValueNumber - 1];
+		let capabilityName = null;
+		if (taskSettings.values) {
+			capabilityName = taskSettings.values[taskValue.ValueNumber - 1];
+		} else {
+			capabilityName = this.getDriver().values[0].name;
+		}
+
 		const capabilitySettings = this.getDriver().values.find(v => v.name == capabilityName);
 		if (!capabilitySettings) {
 			this.log('No capability configured for value', key);