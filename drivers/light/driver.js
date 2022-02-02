d23d66f5820c5917cece43a765d307147f936798
Joolee <570302+Joolee@users.noreply.github.com>
2022-02-02 21:03:05 +0100

    Add support for devices measuring lux

---
+++ drivers/light/driver.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const SensorDriver = require('/lib/SensorDriver.js');
+const Homey = require('homey');
+
+module.exports = class Environment_Driver extends SensorDriver {
+	onInit() {
+		this.taskTypes = [{
+				"name": 'Light/Lux - BH1750',
+				"plugin": 10,
+				"values": ["Lux"]
+			},
+			{
+				"name": 'Light/Lux - TSL2561',
+				"plugin": 15,
+				"values": ["Lux", "Lux_Infrared", "Lux_Broadband"]
+			},
+			{
+				"name": 'Light/Lux - TSL2591 [TESTING]',
+				"plugin": 74,
+				"values": ["Lux", "Lux_Broadband", "Lux_Visible", "Lux_Infrared"]
+			},
+			{
+				"name": 'Light/Lux - TSL2591',
+				"plugin": 74,
+				"values": ["Lux", "Lux_Broadband", "Lux_Visible", "Lux_Infrared"]
+			},
+		]
+
+		this.values = [{
+				"name": "Lux",
+				"capability": "measure_luminance"
+			},
+			{
+				"name": "Lux_Infrared",
+				"capability": "measure_luminance_ir"
+			},
+			{
+				"name": "Lux_Broadband",
+				"capability": "measure_luminance_full"
+			},
+			{
+				"name": "Lux_Visible",
+				"capability": "measure_luminance_visible"
+			}
+		]
+
+		super.onInit();
+	}
+}
