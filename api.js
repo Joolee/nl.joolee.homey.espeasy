09cf5531a5582912a0f534156e057683ab77c793
Joolee <570302+Joolee@users.noreply.github.com>
2023-06-09 21:15:41 +0200

    Update webapi to SDKv3

--- api.js
+++ api.js
@@ -1,18 +1,13 @@
-const Homey = require('homey');
-
-module.exports = [{
-	method: 'GET',
-	path: '/',
-	public: true,
-	fn: (args, callback) => {
+module.exports = {
+	async triggerEvent({homey, query}) {
 		try {
-			if (Homey.app) {
-				Homey.app.units.inbound(args.query, callback);
+			if (homey.app) {
+				homey.app.units.inbound(args.query, callback);
 			} else {
 				console.log('Event received but ignoring because app has not initialized yet');
 			}
 		} catch (error) {
 			console.log("Could not process event", args, error);
 		}
-	}
-}]
+	}	
+}
